b5a8972e1b6cc5b8e5b522b92fc73c507f4ef59c
Osvaldas Beniušis <osvaldas.beniusis@gmail.com>
2024-05-10 11:48:56 +0300

    Add random movement

--- app/Business/Bot/Command/Move/MoveCommand.php
+++ app/Business/Bot/Command/Move/MoveCommand.php
@@ -6,9 +6,20 @@ class MoveCommand
 {
     public function get(array $gameState): MoveParameters
     {
-        return new MoveParameters(
-            x: $gameState['players']['bearer']['position']['x'],
-            y: $gameState['players']['bearer']['position']['y'],
-        );
+        $move = $this->getRandomPossibleMove($gameState);
+
+        return $this->transformParameters($move);
+    }
+
+    private function getRandomPossibleMove(array $gameState): array
+    {
+        $possibleMoves = $gameState['players']['bearer']['possible_moves'];
+
+        return $possibleMoves[array_rand($possibleMoves)];
+    }
+
+    private function transformParameters(array $move): MoveParameters
+    {
+        return new MoveParameters(x: $move['x'], y: $move['y']);
     }
 }
