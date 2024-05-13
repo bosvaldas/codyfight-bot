9d5a7b149a27c4937503ace81edaea0edfb0ec7a
Osvaldas Beniušis <osvaldas.beniusis@gmail.com>
2024-05-13 20:57:53 +0300

    Add other bot configs

--- config/codyfight.php
+++ config/codyfight.php
@@ -1,10 +1,22 @@
 <?php
 
+use Illuminate\Support\Env;
+
 return [
     'bot' => [
         'foo' => [
-            'ckey' => env('BOT_0_CKEY'),
-            'game_mode' => env('BOT_0_GAME_MODE'),
+            'ckey' => Env::getOrFail('BOT_0_CKEY'),
+            'game_mode' => 0,
+            'factory_class_name' => \App\Business\Bots\FooBot\FooBotFactory::class,
+        ],
+        'foo_bar' => [
+            'ckey' => Env::getOrFail('BOT_1_CKEY'),
+            'game_mode' => 0,
+            'factory_class_name' => \App\Business\Bots\FooBot\FooBotFactory::class,
+        ],
+        'bar_baz' => [
+            'ckey' => Env::getOrFail('BOT_2_CKEY'),
+            'game_mode' => 0,
             'factory_class_name' => \App\Business\Bots\FooBot\FooBotFactory::class,
         ],
     ],
